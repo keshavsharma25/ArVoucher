e74f064bcf775dcd7284cc6a0a29de9da41cdc61
Keshav Sharma <skeshav0825@gmail.com>
2023-08-08 23:09:45 +0530

    update claim, deploy voucher

--- src/app/utils/getContract.ts
+++ src/app/utils/getContract.ts
@@ -1,6 +1,7 @@
 import { WarpFactory } from "warp-contracts";
+import { DeployPlugin } from "warp-contracts-plugin-deploy";
 
-export const warp = WarpFactory.forMainnet();
+export const warp = WarpFactory.forMainnet().use(new DeployPlugin());
 
 export async function getContract(transactionId: string) {
   const contract = await warp.contract(transactionId).connect("use_wallet");
